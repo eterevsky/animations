3e8f3d794d63b4549e6ecd9d24f5fd6ab2fd93c9
Oleg Eterevsky <olege@google.com>
2015-04-30 04:23:21 +0300

    Tidy up hexagon code.

--- hexagons.py
+++ hexagons.py
@@ -1,33 +1,45 @@
 import math
 
-SQ3 = math.sqrt(3)
+def rotate(vec, angle_deg):
+  angle = angle_deg * math.pi / 180
+  s = math.sin(angle)
+  c = math.cos(angle)
+  x, y = vec
+  return c * x - s * y, s * x + c * y
 
-verts = [(1/2, 1/(2*SQ3)), (0, 1/SQ3), (-1/2, 1/(2*SQ3)),
-         (-1/2, -1/(2*SQ3)), (0, -1/SQ3), (1/2, -1/(2*SQ3))]
-
-TEMPLATE = """
-AttributeBegin
-  Material "matte" "color Kd" [{color}]
+OBJECT_TEMPLATE = """
+ObjectBegin "hexagon"
   Shape "trianglemesh" "integer indices" [0 1 2  0 2 3  0 3 4  0 4 5]
     "point P" [{points}]
+ObjectEnd
+"""
+
+def base_hexagon():
+  x, y = (1 / 2, 1 / (2 * math.sqrt(3)))
+  points = [x, y, 0]
+  for i in range(5):
+    x, y = rotate((x, y), 60)
+    points.extend([x, y, 0])
+  return OBJECT_TEMPLATE.format(
+      points=' '.join('{:.6f}'.format(p) for p in points))
+
+INSTANCE_TEMPLATE = """
+AttributeBegin
+  Translate {x:.5f} {y:.5f} {z:.5f}
+  NamedMaterial "hex{idx}"
+  ObjectInstance "hexagon"
 AttributeEnd
 """
 
+HSTEP = 0.4
+
 out = open('out/hexagons-geom.lxo', 'w')
+out.write(base_hexagon())
 
 for x in range(-5, 6):
   for y in range(-5, 6):
-    cx, cy = (x + y / 2, y * SQ3 / 2)
-    h = 0.4 * ((x + 2*y) % 3)
-    for dh in range(4):
-      points = []
-      for i in range(6):
-        dx, dy = verts[i]
-        points.append((cx + dx, h + dh * 1.2, cy + dy))
-
-      points_str = '  '.join('{:.4f} {:.4f} {:.4f}'.format(x, y, z)
-                             for x, y, z in points)
-      colors = [(0.902, 0.29, 0.098), (0.38, 0.38, 0.38), (0.271, 0.353, 0.392)]
-      c = colors[(x + 2*y) % 3]
-
-      out.write(TEMPLATE.format(points=points_str, color=' '.join(map(str, c))))
+    cx, cy = (x + y / 2, y * math.sqrt(3) / 2)
+    idx = (x + 2 * y) % 3
+    for h in range(2):
+      out.write(INSTANCE_TEMPLATE.format(
+          idx=idx, x=cx, y=cy, z=-(HSTEP * idx + 3 * HSTEP * h)))
